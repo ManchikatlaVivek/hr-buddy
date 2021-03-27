0f6479dbb736df86cc456417315d206275657e5a
ManchikatlaVivek <vivekmanchikatla1996@gmail.com>
2021-03-27 11:35:05 +0530

    HR Buddy empty app version 0

---
+++ src/main/java/com/viveksolutions/hrbuddy/HrBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.viveksolutions.hrbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viveksolutions/hrbuddy/HrBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viveksolutions.hrbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
